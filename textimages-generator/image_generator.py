daf9d8920ad3cce215ece04f3cb66e732fd063cb
Simon Immer <118479244+NoknokII@users.noreply.github.com>
2024-03-31 10:20:04 +0200

    Added folder build method and started with the generate_new_image method

--- textimages-generator/image_generator.py
+++ textimages-generator/image_generator.py
@@ -4,16 +4,23 @@ import os
 
 
 class ImageGenerator:
-    def __init__(self, font_path=".", output_folder=".", image_width=500, image_height=500, font_size=24, font_color="BLACK"):
+    def __init__(self, font_folder="fonts", output_folder="images", image_dimensions=(500, 500), font_size=24,
+                 font_color=(255, 255, 255)):
         self.output_folder = output_folder
-        self.image_width = image_width
-        self.image_height = image_height
-        self.font_list = os.listdir(font_path)
+        self.font_folder = font_folder
+        self.image_dimensions = image_dimensions
+        self.font_list = os.listdir(font_folder)
         self.font_size = font_size
         self.font_color = font_color
 
-    def generate(self):
-        return self.font_list
+    def build_folders(self):
+        if not os.path.exists(self.output_folder):
+            os.makedirs(self.output_folder)
+        if not os.path.exists(self.font_folder):
+            os.makedirs(self.font_folder)
 
-
-
+    def generate_new_image(self):
+        font = ImageFont.truetype(random.choice(self.font_list), random.randint(20, 40))
+        non_diacritic_text = textlist
+        diacritic_text = diacritic_text_list
+        Image.new("RGB", (self.image_dimensions[0], self.image_dimensions[1]), self.font_color)
